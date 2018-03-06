28fb9ae0e85987b8ff17e3f82332dcccbc2e2874
Steven <sbruno636@gmail.com>
2018-03-06 16:53:23 -0600

    wrote primary functions in monkey.py, simulate_monkeys() needs work

---
+++ monkey.py
@@ -0,0 +1,55 @@
+# OBJECTIVE: generate a random 28 char string, compare it to the target string,
+#            score the generated string based on similarity to target, repeat the process until there is a 100% match
+#            https://runestone.academy/runestone/static/pythonds/Introduction/DefiningFunctions.html
+
+import random
+import string
+
+
+target = "methinks it is like a weasel" #target string to which we will compare our generated strings
+
+
+def generate_string():
+    test_string = []
+    for i in range(28):
+        test_string.append(random.choice(string.ascii_lowercase + " "))
+    return test_string
+
+
+def score_string(target_string, attempt):
+    score = 0
+    for i in range(len(target_string)):
+        if target_string[i] == attempt[i]:
+            score += 1
+    return score
+
+
+# repeatedly calls generate and score until there is a 100% match
+def simulate_monkeys():
+    counter = 0
+    best_score = 0
+    best_string = ""
+    trial = generate_string()
+    trial_score = score_string(target, trial)
+    while trial_score != 28:
+        trial = generate_string()
+        trial_score = score_string(target, trial)
+        counter += 1
+        if trial_score > best_score:
+            best_score = trial_score
+            best_string = trial
+        if counter % 1000 == 0:
+            print(best_score, best_string)
+
+
+simulate_monkeys()
+
+
+
+
+
+
+
+
+
+
