4cef0fb96bbd567a48b1fe82f8bada80fa25c009
unknown <sbruno636@gmail.com>
2018-04-20 11:32:07 -0500

    created fraction.py

---
+++ fraction.py
@@ -0,0 +1,11 @@
+# This program defines a Fraction class and various associated methods. 
+# this is merely instructional
+
+
+class Fraction:
+
+	def __init__(self,top,bottom): #constructor method
+
+		self.num = top
+		self.den = bottom
+
