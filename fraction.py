3975442885ddf2640c8ee4480897a2f7cde4282e
unknown <sbruno636@gmail.com>
2018-04-20 11:45:25 -0500

    added __str__ method override

--- fraction.py
+++ fraction.py
@@ -1,11 +1,14 @@
 # This program defines a Fraction class and various associated methods. 
-# this is merely instructional
+# this is merely instructional and not intended to be executable
 
 
 class Fraction:
 
-	def __init__(self,top,bottom): #constructor method
+    def __init__(self,top,bottom): #constructor method
 
-		self.num = top
-		self.den = bottom
+	    self.num = top
+	    self.den = bottom
 
+    def __str__(self):
+        return str(self.num)+"/"+str(self.den)
+    
