c580ad21585256623aff87e446583b154b974102
Steven Bruno <sbruno636@gmail.com>
2018-05-06 13:01:10 -0500

    integrated pyplot to visualize time complexity

--- min_number.py
+++ min_number.py
@@ -1,21 +1,23 @@
-# Prompt: Write two functions to find the minimum number in a list. The first function should compare each number to
-# every other number in the list O(n^2). The second function should be linear O(n).
 
+"""
+Prompt: Write two functions to find the minimum number in a list. The first function should compare each number to
+every other number in the list O(n^2). The second function should be linear O(n). From Miller and Ranum.
+Author: Steven Bruno
+"""
 
-import random
-
-
-num_list = random.sample(range(0,100),10) #creates list of ten random integers from 0 to 99
+from random import randrange
+import timeit
+import matplotlib.pyplot as plt
 
 
 def get_min_num_On2(some_list):
     for i in some_list:
         is_smallest = True
-        while is_smallest == True:
+        if is_smallest == True:
             for j in some_list:
                 if j < i:
                     is_smallest = False
-            if is_smallest:
+            if is_smallest == True:
                 return i
 
 
@@ -29,6 +31,24 @@ def get_min_num_On(some_list):
     return min_num
 
 
-print(get_min_num_On2(num_list))
+list_range = [] #range for x axis in time complexity plot
+On2_time = []
+On_time = []
+for list_size in range(1, 101):
+    list_range.append(list_size)
+    alist = [randrange(101) for i in range(list_size)]
+    t1 = timeit.Timer("get_min_num_On2(alist)", "from __main__ import get_min_num_On2, alist")
+    t2 = timeit.Timer("get_min_num_On(alist)", "from __main__ import get_min_num_On, alist")
+    On2_time.append(t1.timeit(number=1000))
+    On_time.append(t2.timeit(number=1000))
 
-print(get_min_num_On(num_list))
+plt.plot(list_range, On2_time, 'b', label="On2")
+plt.plot(list_range, On_time, 'r', label="On")
+
+plt.xlabel('list size')
+plt.ylabel('function runtime (ms)')
+plt.title('time complexity of linear and exponential functions')
+
+plt.legend()
+
+plt.show()
