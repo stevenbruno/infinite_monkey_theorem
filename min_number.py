dec340121195eaf248f322eabd09c6a69188be8a
Steven Bruno <sbruno636@gmail.com>
2018-05-06 18:33:45 -0500

    updated list range for faster execution

--- min_number.py
+++ min_number.py
@@ -1,13 +1,14 @@
-
 """
 Prompt: Write two functions to find the minimum number in a list. The first function should compare each number to
 every other number in the list O(n^2). The second function should be linear O(n). From Miller and Ranum.
 Author: Steven Bruno
+To Do: progress bar
 """
 
 from random import randrange
 import timeit
 import matplotlib.pyplot as plt
+import sys
 
 
 def get_min_num_On2(some_list):
@@ -31,24 +32,28 @@ def get_min_num_On(some_list):
     return min_num
 
 
-list_range = [] #range for x axis in time complexity plot
-On2_time = []
-On_time = []
-for list_size in range(1, 101):
-    list_range.append(list_size)
-    alist = [randrange(101) for i in range(list_size)]
-    t1 = timeit.Timer("get_min_num_On2(alist)", "from __main__ import get_min_num_On2, alist")
-    t2 = timeit.Timer("get_min_num_On(alist)", "from __main__ import get_min_num_On, alist")
-    On2_time.append(t1.timeit(number=1000))
-    On_time.append(t2.timeit(number=1000))
+list_range = []  # range for x axis in time complexity plot
+On2_time = []    # for storing On2 times for plot
+On_time = []     # for storing On times for plot
 
-plt.plot(list_range, On2_time, 'b', label="On2")
-plt.plot(list_range, On_time, 'r', label="On")
 
-plt.xlabel('list size')
-plt.ylabel('function runtime (ms)')
-plt.title('time complexity of linear and exponential functions')
+if __name__ == '__main__':
+    for list_size in range(20, 501, 20):
+        list_range.append(list_size)
+        alist = [randrange(101) for i in range(list_size)]
+        t1 = timeit.Timer("get_min_num_On2(alist)", "from __main__ import get_min_num_On2, alist")
+        t2 = timeit.Timer("get_min_num_On(alist)", "from __main__ import get_min_num_On, alist")
+        On2_time.append(t1.timeit(number=1000))
+        On_time.append(t2.timeit(number=1000))
 
-plt.legend()
 
-plt.show()
+    plt.plot(list_range, On2_time, 'bo', label="On2")
+    plt.plot(list_range, On_time, 'ro', label="On")
+
+    plt.xlabel('list size')
+    plt.ylabel('function runtime (ms)')
+    plt.title('time complexity of linear and exponential functions')
+
+    plt.legend()
+
+    plt.show()
