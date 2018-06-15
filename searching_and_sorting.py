8f2f1e58ae47ef5d4f5271e8e09cc2a7dd64170e
unknown <sbruno636@gmail.com>
2018-06-15 11:42:20 -0500

    wrote bubble and selection sort functions

--- searching_and_sorting.py
+++ searching_and_sorting.py
@@ -52,3 +52,35 @@ if __name__ == '__main__':
 	testlist = [0, 1, 2, 8, 14, 18, 36, 101]
 	print(binarySearch(testlist, 12))
 	print(recBinarySearch(testlist, 0, len(testlist)-1, 12))
+
+
+# Short Bubble Sort (called short because potential to finish early)
+
+def shortBubbleSort(alist):
+	exchanges - True
+	passnum = len(alist)-1
+	while passnum > 0 and exchanges:
+		exchanges = False
+		for i in range(passnum):
+			if alist[i]>alist[i+1]:
+				exchanges = True
+				temp = alist[i]
+				alist[i] = alist[i+1]
+				alist[i+1] = temp
+			passnum = passnum-1
+
+
+# Selection Sort
+
+def selectionSort(alist):
+	for fillslot in range(len(alist)-1, 0, -1):
+		positionOfMax = 0
+		for location in range(1, fillslot+1):
+			if alist[location] > alist[positionOfMax]:
+				positionOfMax = location
+
+		temp = alist[fillslot]
+		alist[fillslot] = alist[positionOfMax]
+		alist[positionOfMax] = temp
+
+
