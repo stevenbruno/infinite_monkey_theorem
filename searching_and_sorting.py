80fadb0a0b16749eca8d246748c9a4b6aa7bc4c5
unknown <sbruno636@gmail.com>
2018-06-18 11:33:18 -0500

    wrote insertion sort function

--- searching_and_sorting.py
+++ searching_and_sorting.py
@@ -84,3 +84,18 @@ def selectionSort(alist):
 		alist[positionOfMax] = temp
 
 
+# Insertion Sort
+
+def insertionSort(alist):
+	for index in range(1, len(alist)):
+
+		current_value = alist[index]
+		position = index
+
+	while position > 0 and alist[position - 1] > alist[position]:
+		alist[position] = alist[position-1]
+		position -= 1
+
+	alist[position] = current_value
+	
+
