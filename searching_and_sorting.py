290f549d51f4150a1ead0243c8a7c7c3e93291ea
Steven Bruno <sbruno636@gmail.com>
2018-07-22 18:53:54 -0500

    wrote quick sort algorithm

--- searching_and_sorting.py
+++ searching_and_sorting.py
@@ -118,7 +118,7 @@ def gapInsertionSort(alist, start, gap):
 		alist[position] = currentvalue
 
 
-# Merge Sort O(nlogn)
+# Merge Sort O(nlogn) (except the slice makes this an underestimate)
 
 def mergeSort(alist):
     print("Splitting ",alist)
@@ -152,9 +152,50 @@ def mergeSort(alist):
             j=j+1
             k=k+1
     print("Merging ",alist)
-	
+
+
+# Quick Sort: best case O(nlogn), worst close to O(n2), sorts in place, can be improved with median of three 
+def quickSort(alist):
+   quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+   if first<last:
+
+       splitpoint = partition(alist,first,last)
+
+       quickSortHelper(alist,first,splitpoint-1)
+       quickSortHelper(alist,splitpoint+1,last)
+
+def partition(alist,first,last):
+   pivotvalue = alist[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = alist[leftmark]
+           alist[leftmark] = alist[rightmark]
+           alist[rightmark] = temp
+
+   temp = alist[first]
+   alist[first] = alist[rightmark]
+   alist[rightmark] = temp
+
+
+   return rightmark
 
 if __name__ == '__main__':
 	alist = [54,26,93,17,77,31,44,55,20]
-	mergeSort(alist)
+	quickSort(alist)
 	print(alist)
