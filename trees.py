0f20a0ef1df1639e5d1598ee4f200d114a13d669
Steven Bruno <sbruno636@gmail.com>
2018-07-31 21:53:59 -0500

    wrote BinaryTree class

---
+++ trees.py
@@ -0,0 +1,41 @@
+# This code demonstrates various tree and tree algorithm implementations. 
+# Credit to Miller and Ranum, Problem Solving with Algorithms and Data Structures Using Python
+
+
+# Binary Tree
+
+class BinaryTree:
+    def __init__(self,rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+    def insertLeft(self,newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self,newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self,obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
+
+
