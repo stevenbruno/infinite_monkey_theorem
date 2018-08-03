a899ee523d81cb23e37470e79122a6359480f4d3
Steven Bruno <sbruno636@gmail.com>
2018-08-02 22:11:49 -0500

    wrote basic parse tree

--- trees.py
+++ trees.py
@@ -39,3 +39,39 @@ class BinaryTree:
         return self.key
 
 
+# Parse Tree
+
+from pythonds.basic.stack import Stack 
+from pythonds.trees.binaryTree import BinaryTree
+
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRootVal(i) 
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild
+        
+        elif i == ')':
+            currentTree = pStack.pop()
+        
+        elif i not in ['+', '-', '*', '/', ')']:
+            try: 
+                currentTree.setRootVal(int(i))
+                parent = pStack.pop()
+                currentTree = parent
+            except ValueError:
+                raise ValueError('token {} is not a valid integer'.format(i))
+        
+    return eTree
