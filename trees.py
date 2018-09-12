dbf37bd72f59092150520afd0707ac48bf2c2d99
Steven Bruno <stevenbruno@Junior-Designer-MacBook.local>
2018-09-12 16:37:00 -0500

    created binary heap class

--- trees.py
+++ trees.py
@@ -135,3 +135,25 @@ def printexp(tree):
         sVal +=  str(tree.getRootVal())
         sVal += printexp(tree.getRightChild()) + ')'
     return sVal
+
+
+# Binary Heap 
+
+class BinHeap:
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def percUp(self, i):
+        while i//2 > 0:
+            if self.heapList[i] < self.heapList[i//2]:
+                tmp = self.heapList[i//2]
+                self.heapList[i//2] = self.heapList[i]
+                self.heapList[i] = tmp
+            i = i//2
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize)
+
