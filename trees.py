3ece88e0d9ff2ab4d3d5197a50ba1a43ab7dbf69
Steven Bruno <stevenbruno@Junior-Designer-MacBook.local>
2018-09-12 16:54:32 -0500

    wrote percdown and minchild methods

--- trees.py
+++ trees.py
@@ -157,3 +157,19 @@ class BinHeap:
         self.currentSize = self.currentSize + 1
         self.percUp(self.currentSize)
 
+    def percDown(self, i):
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
+    
+    def minChild(self, i):
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i*2] < self.heapList[i * 2 + 1]:
+                return i * 2
+            else: return i * 2 + 1
