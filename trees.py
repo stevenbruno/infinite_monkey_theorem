1056323919ef495e32f7ae1c5ae2817e87821d0a
Steven Bruno <stevenbruno@Junior-Designer-MacBook.local>
2018-09-13 09:28:13 -0500

    wrote delMin method

--- trees.py
+++ trees.py
@@ -173,3 +173,12 @@ class BinHeap:
             if self.heapList[i*2] < self.heapList[i * 2 + 1]:
                 return i * 2
             else: return i * 2 + 1
+    
+    def delMin(self):
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
+
